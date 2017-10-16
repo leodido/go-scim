57435ef6853e689cf9138d0ae9dbb7c65cc92e56
alessandro <sandrone.lb@gmail.com>
2017-10-16 14:54:33 +0200

    WIP: Working on get by username

--- handlers/account_password.go
+++ handlers/account_password.go
@@ -9,6 +9,26 @@ import (
 
 func CreatePasswordResetRequestHandler(r shared.WebRequest, server ScimServer, ctx context.Context) (ri *ResponseInfo) {
 	ri = newResponse()
+	sch := server.InternalSchema(shared.PasswordResetRequestUrn)
+	repo := server.Repository(shared.UserResourceType)
+
+	resource, err := ParseBodyAsResource(r)
+
+	err = server.ValidateType(resource, sch, ctx)
+	ErrorCheck(err)
+
+	username := resource.GetData()["username"].(map[string]interface{})["username"].(string)
+
+	user, err := repo.GetByUsername(username)
+	ErrorCheck(err)
+
+	id := user.GetData()["id"].(map[string]interface{})["id"].(string)
+	version := user.GetData()["version"].(map[string]interface{})["version"].(string)
+
+	user["password"] = resource.GetData()["password"]
+
+	err = repo.Update(id, version, user)
+	ErrorCheck(err)
 
 	ri.Status(http.StatusCreated)
 	return
